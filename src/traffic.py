4e8cd45a295acd112d8da8446b54960f497d83ca
a097245@gmail.com <a097245@gmail.com>
2018-12-15 22:59:52 +0800

    add train info

--- src/traffic.py
+++ src/traffic.py
@@ -1,6 +1,73 @@
+from dominate import document
+from dominate.tags import *
+from dominate.util import raw
+from bs4 import BeautifulSoup
+import requests
 from linebot.models import (
     TextSendMessage
 )
 
+def textMessageTemplate(message):
+    txt = ''
+    for (key, value) in message.items():
+        print(value)
+        if key == 'text':
+            txt += value
+        if key == 'link':
+            txt += str(a(value, href=value))
+        txt += '<br>'
+
+    return txt + '<br>'
+
+
+def trainInfo():
+    resp = requests.get(
+        transport['火車']['website'],
+        {},
+    )
+    resp.encoding = 'UTF-8'
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    tags = soup.select('#DG tr')
+
+    message = []
+    for tag in tags:
+        try:
+            message.append({
+                'text': tag.select_one('span')['title'],
+                'link': 'https://www.railway.gov.tw/tw/' + tag.select_one('a')['href']
+            })
+        except:
+            print('error')
+
+    return message
+
+
+def transportation(request):
+    parameter = request.split(',')
+
+
 def reply(request):
-    return TextSendMessage(text=request)
+
+    messages = transport['火車']['info']()
+
+    result = ''
+    for message in messages:
+        result += textMessageTemplate(message)
+
+    return TextSendMessage(text=result)
+
+transport = {
+        '火車': {
+            'website': 'https://www.railway.gov.tw/tw/news.aspx?n=21560',
+            'info': trainInfo
+        },
+        '公車': {
+            'website': ''
+        },
+        '高鐵': {
+            'website': ''
+        },
+        '捷運': {
+            'website': ''
+        },
+}
