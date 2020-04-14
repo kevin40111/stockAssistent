75f7bb3fc7d0e98874ed67431ad9aa64f5899b0e
a097245@gmail.com <a097245@gmail.com>
2020-04-14 17:27:42 +0800

    stock init

--- README.md
+++ README.md
@@ -1,64 +1,12 @@
 # **Reply**
 Reply 回傳改成Ｍessage物件的集合
 
-# **環境建置**
-[開發需求工具](https://hackmd.io/s/rJLWLWx_Q#Install-pip)
-
 # **Line message**
 [linbot-messate-object](https://github.com/line/line-bot-sdk-python#user-content-message-objects)
 [message data struct](https://ithelp.ithome.com.tw/articles/10198142)
 
-### Step1 複製程式碼
+### 複製程式碼
 ```
 ##終端機打開進入到你想放程式的目錄##
-git clone https://github.com/kevin40111/ntust-hci-group5.git
-```
-
-### Step2 放入你自己的 .env 擋到複製下來的目錄中
-
-### Step3 安裝依賴的套件
-```
-##python要先安裝pipenv##
-pipenv install
-```
-
-### Step4 開啟服務
-```
-pipenv shell
-honcho start -f Procfile.dev
-line-simulator
-```
-- - - -
-
-# ##撰寫方式##
-### 需求
-實作各檔案 reply 的方法，編寫要回傳的訊息，回傳資料型態為字串．
-
-### 對應
-目前已將關鍵字對應的檔案添加進map中
-```
-map = {
-        '災害': 'disaster',
-        '預防': 'prevention',
-        '檢測': 'detection',
-        '交通': 'traffic',
-        '歷史資料': 'history',
-    }
-```
-
-### 測試
-在模擬器中輸入  歷史:  <parameter> ， 目前使用 : 符號來決定功能模組．
-- - - -
-
-# **上傳檔案**
-### 建立分支
-```
-git checkout -b <分之名稱>
-```
-
-### 推到git hub上
-```
-##origin 一般是遠端pull下來預設的名稱##
-git remeote -v ##查看遠儲存體位置##
-git push origin
+git clone https://github.com/kevin40111/tockAssistent.git
 ```
--- line.py
+++ line.py
@@ -1,32 +1,32 @@
-from flask import Flask, request, abort
-import os
-import sys
-sys.path.append('./src')
-
-from linebot import (
-    LineBotApi, WebhookHandler
+import psycopg2
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+from linebot import (
+    LineBotApi, WebhookHandler
 )
+from flask import Flask, request, abort
+import sys
+sys.path.append('./src')
 
-import psycopg2
 
 app = Flask(__name__)
 
-line_bot_api = None
+line_bot_api = LineBotApi(
+    "Ioj/1iUb4Bh1rUUqoc0DXWyAsgarF0oOjHBiP/e/SxCJ+V3i5ZGdWpUWAcF6Mr/rACK9LTalK3MJfZkXy4omfnrsoUS9dKoyOJlQCV/q4nftBSQgZ0WViYgZqEBMw5s+fntZQvJLrhnwpNz4LpOo8QdB04t89/1O/w1cDnyilFU=")
 
-if os.environ.get('ENVIRONMENT') == 'development':
-    line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'), 'http://localhost:8080')
-else:
-    line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
-
-handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
+handler = WebhookHandler("b0532bf3b9affff65bbced87ba607b93")
 
 __import__('push').start()
+
+
+@app.route("/hellow", methods=['GET'])
+def hellow():
+    return 'Hellow World'
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -39,49 +39,36 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
-
 def handle_message(event):
-    map = {
-        '災害': 'disaster',
-        '預防': 'prevention',
-        '檢測': 'detection',
-        '交通': 'traffic',
-        '歷史資料': 'history',
-        '使用者': 'users',
-        '推播': 'broadcast'
-    }
+    # map = {
+    #     '災害': 'disaster',
+    #     '預防': 'prevention',
+    #     '檢測': 'detection',
+    #     '交通': 'traffic',
+    #     '歷史資料': 'history',
+    #     '使用者': 'users',
+    #     '推播': 'broadcast'
+    # }
 
-    try:
-        conn = psycopg2.connect(
-            dbname="group5pg", user="postgres", host="hci.dianalab.net", password="c8eccf33282708be620bb689dd54c2e8", port="10715"
-        )
-        cur = conn.cursor()
-        query = "insert into users values ('{}');".format(event.source.user_id)
-        cur.execute(query)
-        conn.commit()
-        conn.close()
-    except Exception:
-        print(Exception)
-
-    try:
-        spelate = ':' if event.message.text.find(':') != -1 else '：'
-        split = event.message.text.split(spelate)
-        position = event.message.text.find(spelate)
-
-        request = event.message.text[position+1:]
-        module = __import__(map[split[0]])
-
-        #回傳一個message的物件集合 size between 1 and 5
-        messageOBJ_array = module.reply(request)
-
-    except:
-        messageOBJ_array = TextSendMessage(text='請輸入符合的參數結構 ex 交通:火車')
+    # try:
+    #     conn = psycopg2.connect(
+    #         dbname="group5pg", user="postgres", host="hci.dianalab.net", password="c8eccf33282708be620bb689dd54c2e8", port="10715"
+    #     )
+    #     cur = conn.cursor()
+    #     query = "insert into users values ('{}');".format(event.source.user_id)
+    #     cur.execute(query)
+    #     conn.commit()
+    #     conn.close()
+    # except Exception:
+    #     print(Exception)
 
     line_bot_api.reply_message(
         event.reply_token,
-        messageOBJ_array
+        TextSendMessage(text=event.message.text)
     )
+
 
 if __name__ == "__main__":
     app.run()
